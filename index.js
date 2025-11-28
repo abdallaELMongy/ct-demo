e5ccb747f3ae0630223eceeb6ee7c90c4bc0f87a
Abdallah Mohamed Farid <116508319+abdallaELMongy@users.noreply.github.com>
2025-11-28 19:52:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function sum(a,b){ return a+b; }
+function sum(a, b) {
+  // Validation: do not accept zero values
+  if (a === 0 || b === 0) {
+    throw new Error('Inputs must be non-zero');
+  }
+  return a + b;
+}
+
 module.exports = { sum };
