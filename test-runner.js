36d89ba9d77ae290e024e407b8b062c3ac50ba64
Abdallah Mohamed Farid <116508319+abdallaELMongy@users.noreply.github.com>
2025-11-28 20:40:17 +0200

    Update test-runner.js

--- test-runner.js
+++ test-runner.js
@@ -1,14 +1,1 @@
-const fs = require("fs");
-const path = require("path");
-
-// Path to the test folder (relative to test-runner.js)
-const testDir = path.join(__dirname, "test");
-
-// Read all files in the test folder
-fs.readdirSync(testDir).forEach((file) => {
-  if (file.endsWith(".test.js")) {
-    const filePath = path.join(testDir, file);
-    console.log("Running:", file);
-    require(filePath);  // Correct absolute path
-  }
-});
+require('./test/sum.test.js'
