c1503daeeafebd97829e4b576bf21f30946d49bc
Abdallah Mohamed Farid <116508319+abdallaELMongy@users.noreply.github.com>
2025-11-28 20:27:10 +0200

    Update test-runner.js

--- test-runner.js
+++ test-runner.js
@@ -1,1 +1,12 @@
-require('./test/*');
+const fs = require("fs");
+const path = require("path");
+
+// Folder where your tests exist (change if needed)
+const testDir = "./test";
+
+fs.readdirSync(testDir).forEach((file) => {
+  if (file.endsWith(".test.js")) {
+    console.log("Running:", file);
+    require(path.join(testDir, file));
+  }
+});
